bfd08ce75a36975904a6c89867610232acbf9284
Neeky <1721900707@qq.com>
2017-07-02 18:13:27 +0800

    17.07.02 完成对glod数据的收集

--- glod/models.py
+++ glod/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime,date
 
 # Create your models here.
+class GlodPrice(models.Model):
+    pushDate=models.DateTimeField(primary_key=True)
+    contract=models.CharField(max_length=8)
+    openingPrice=models.DecimalField(max_digits=10,decimal_places=6)
+    closingPrice=models.DecimalField(max_digits=10,decimal_places=6)
+    highestPrice=models.DecimalField(max_digits=10,decimal_places=6)
+    minimumPrice=models.DecimalField(max_digits=10,decimal_places=6)
+    
+    def fromJson(self,jdt):
+        dtn=datetime.now()
+        dt=date(year=dtn.year,month=dtn.month,day=dtn.day)
+        self.pushDate=dt
+        self.contract=jdt['contract']
+        self.openingPrice=jdt['openingPrice']
+        self.closingPrice=jdt['closingPrice']
+        self.highestPrice=jdt['highestPrice']
+        self.minimumPrice=jdt['minimumPrice']
--- glod/views.py
+++ glod/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import GlodPrice
+import json
+from datetime import datetime,date
 # Create your views here.
+
+def webClientPostHandler(request):
+    try:
+        data=json.loads(request.body.decode('utf8'))
+        year=datetime.now().year
+        month=datetime.now().month
+        day=datetime.now().day
+        lastRows=GlodPrice.objects.filter(pushDate__istartswith='{0}-{1}-{2}'.format(year,month,day))
+        if len(lastRows) == 0 and data['closingPrice'] != -1:
+            gp=GlodPrice()
+            gp.fromJson(data)
+            gp.save()
+            return HttpResponse("ok")
+        return HttpResponse("today is weekend can not upload data to database (glod)")
+    except Exception as e:
+        print(e)
+        return HttpResponse("exception has occur in glod.view.webClientPostHandler")
