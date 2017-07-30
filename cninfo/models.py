dc9ccde485a68b268fd22b0a09ed9f2170a05611
Neeky <1721900707@qq.com>
2017-07-30 10:56:03 +0800

    17.07.30.1

--- cninfo/models.py
+++ cninfo/models.py
@@ -4,24 +4,24 @@ from datetime import datetime
 # Create your models here.
 
 class Company(models.Model):
-    name=models.CharField(help_text='公司名',max_length=16)
+    name=models.CharField(help_text='公司名',max_length=32)
     stockCode=models.CharField(help_text='证券代码',max_length=6,primary_key=True)
-    section=models.CharField(help_text='版块 中小企业板|创业板 ...',max_length=8)
+    section=models.CharField(help_text='版块 中小企业板|创业板 ...',max_length=32)
     mainPage=models.CharField(help_text='公司在cninfo中的主页地址',max_length=128)
 
     #以下是公司简介中的内容
-    fullName=models.CharField(max_length=16,default='',help_text='公司全称')
-    briefName=models.CharField(max_length=16,default='',help_text='公司简称')
+    fullName=models.CharField(max_length=32,default='',help_text='公司全称')
+    briefName=models.CharField(max_length=32,default='',help_text='公司简称')
     enlishName=models.CharField(max_length=64,default='',help_text='英文名称')
     registeredAddress=models.CharField(max_length=64,default='',help_text='注册地址')
-    legalPerson=models.CharField(max_length=16,default='',help_text='法定代表人')
-    secretaries=models.CharField(max_length=16,default='',help_text='董秘')
-    registeredCapital=models.DecimalField(max_digits=16,decimal_places=4,default=0,help_text='注册资本')
+    legalPerson=models.CharField(max_length=32,default='',help_text='法定代表人')
+    secretaries=models.CharField(max_length=32,default='',help_text='董秘')
+    registeredCapital=models.DecimalField(max_digits=32,decimal_places=4,default=0,help_text='注册资本')
     industry=models.CharField(max_length=32,default='',help_text='行业种类')
     postalCode=models.CharField(max_length=8,default='',help_text='邮政编码')
-    companyPhone=models.CharField(max_length=16,default='',help_text='公司电话')
-    companyFax=models.CharField(max_length=16,default='',help_text='公司传真')
-    companyWebsite=models.CharField(max_length=32,default='',help_text='公司网址')
+    companyPhone=models.CharField(max_length=32,default='',help_text='公司电话')
+    companyFax=models.CharField(max_length=32,default='',help_text='公司传真')
+    companyWebsite=models.CharField(max_length=64,default='',help_text='公司网址')
     listingTime=models.DateTimeField(default='1000-01-01 01:01:01',help_text='上市时间')
     prospectusTime=models.DateTimeField(default='1000-01-01 01:01:01',help_text='招股时间')
     issuedQuantity=models.DecimalField(max_digits=12,decimal_places=0,default=0,help_text='发行数量')
@@ -30,7 +30,7 @@ class Company(models.Model):
     issueMode=models.CharField(max_length=16,default='',help_text='发行方式')
     leadUnderwrite=models.CharField(max_length=16,default='',help_text='主承销商')
     issueRecommender=models.CharField(max_length=32,default='',help_text='上市推荐人')
-    sponsor=models.CharField(max_length=16,default='',help_text='保荐机构')    
+    sponsor=models.CharField(max_length=32,default='',help_text='保荐机构')    
     #lastupdated 用于表示最近一次更新的时间
     lastUpdate=models.DateTimeField(default=datetime.now(),help_text='招股时间')
 
